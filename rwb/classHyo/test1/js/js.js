62a4c37b99346ed2a570eb4855d73bbc64fbea60
HYE <hyebonny1106@naver.com>
2022-06-28 17:14:38 +0900

    0628.02

--- rwb/classHyo/test1/js/js.js
+++ rwb/classHyo/test1/js/js.js
@@ -3,4 +3,22 @@ function hideDiv (){
 
   }
   self.setTimeout("hideDiv()",6000); 
+  
 
+// window.onload = function() {
+//     function hideDiv (){
+//         document.getElementById("smokyWrap").style.display="none"; 
+//     }        
+//     self.setTimeout("hideDiv()", 6000);
+// }
+
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".contPortfolio_imgWrap>a"); //메뉴 링크
+//window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+window.addEventListener("scroll", cursor);
+window.addEventListener("mousemove", cursor);
+//커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
+function cursor(e) {
+  mouseCursor.style.left = e.pageX + "px";
+  mouseCursor.style.top = e.pageY - scrollY + "px";
+}
