da1133c4694f7e1429c4be743ce7d7767cf6556f
HYE <hyebonny1106@naver.com>
2022-07-08 12:20:11 +0900

    0708.04

--- rwb/classHyo/test1/js/script.js
+++ rwb/classHyo/test1/js/script.js
@@ -1,3 +1,4 @@
+//----------------------- logEvent
 let menuEvent = document.getElementById('menu');
 let smokyWrapEvent = document.getElementById('smokyWrap');
 let posterEvent = document.getElementById('posterCover');
@@ -11,21 +12,17 @@ let posterEvent = document.getElementById('posterCover');
   self.setTimeout("hideDiv()",6000); 
 
 
-
+//----------------------- mouse
 
 let mouseCursor = document.querySelector(".cursor");
-// let navLinks = document.querySelectorAll(".contArt_imgWrap>a"); //메뉴 링크
-// //window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
 window.addEventListener("scroll", cursor);
 window.addEventListener("mousemove", cursor);
-//커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
 function cursor(e) {
   mouseCursor.style.left = e.pageX + "px";
   mouseCursor.style.top = e.pageY - scrollY + "px";
 }
 
-
-// //----------------------- map
+//----------------------- map
 
 let map_Opbt = document.getElementById('mapBtn');
 let map = document.getElementById('mapOn');
