aeb87c5937c8b4cb9ebf2cdf293026e72a38c9b8
HYE <hyebonny1106@naver.com>
2022-09-13 15:33:59 +0900

    0913.05

--- oneandthree/js/index_js.js
+++ oneandthree/js/index_js.js
@@ -1,3 +1,4 @@
+// -----width=690(모바일)경우 메뉴변화-----
 let openMenu = document.querySelector('.menu_btn'),
     slide = document.querySelector('nav'),
     closeMenu = document.querySelector('.menuClose'),
@@ -33,7 +34,27 @@ for(i=0; i<menulistClick.length; i++) {
         };
     });
 }
-// wrapTopButton_ 탑버튼 
+
+
+// -----main_Imgbox_ 이미지 슬라이드쇼 -----
+let main_banner_Img= [
+    "https://i.ibb.co/QDh4wMc/main-PC-1920x440-20201006040046969.jpg",
+    "https://i.ibb.co/fXxC4j1/main-PC-1920x440-20220722110659575.jpg",
+    "https://i.ibb.co/wzG57vQ/main-PC-1920x440-20220829043050726.jpg",
+    "https://i.ibb.co/bWHd72n/main-PC-1920x440-20220830013330280.jpg"
+]    
+let cnt = 0;
+function chage_banner_Img() {                       
+    document.querySelector('.main_banner_Img').setAttribute('src', main_banner_Img[cnt]);
+    // document.querySelector('.main_banner_Imgbox>a').setAttribute('href', 이미지 자세히보기 주소);
+    console.log(cnt);
+    cnt = cnt + 1;        
+    if( cnt == 4) {
+        cnt = 0;
+    };        
+}
+setInterval(chage_banner_Img, 2000);
+// ----wrapTopButton_ 탑버튼 -----
 const topBtn = document.getElementById('topBtn');
 topBtn.onclick = () => {
     window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
